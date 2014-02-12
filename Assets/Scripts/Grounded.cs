fa29362b110410556df90433c8e498da2af44746
Zephyr6 <murlocholmes@gmail.com>
2014-02-12 00:41:02 -0700

    Player stuff

---
+++ Assets/Scripts/Grounded.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grounded : MonoBehaviour {
+    public Transform groundCheck;
+    public bool IsGrounded = false;
+
+	void Update () {
+        IsGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        if(!IsGrounded)
+            IsGrounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Squamp"));
+	}
+}

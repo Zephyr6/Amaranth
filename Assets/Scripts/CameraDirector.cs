ba76be32a804836ec6f5882babd504f07d98f4d6
Zephyr6 <murlocholmes@gmail.com>
2014-02-16 11:29:30 -0700

    Some changes to the dirt tile. Changes to the test scene. Added the Camera Director script.

---
+++ Assets/Scripts/CameraDirector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraDirector : MonoBehaviour {
+
+    public float yOffset = 1.3F;
+    public float yBase = 0F;
+
+	// Use this for initialization
+	void Start () {
+        if(GameObject.FindGameObjectWithTag("Player") != null)
+        {
+            var playerObject = GameObject.FindGameObjectWithTag("Player");
+            transform.position = new Vector3(playerObject.transform.position.x, playerObject.transform.position.y + yOffset, transform.position.z);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var playerObject = GameObject.FindGameObjectWithTag("Player");
+
+        if (GameObject.FindGameObjectWithTag("Player") != null)
+            transform.position = new Vector3(playerObject.transform.position.x, playerObject.transform.position.y + yOffset, transform.position.z);
+
+        if (transform.position.y <= yBase)
+            transform.position = new Vector3(playerObject.transform.position.x, yBase, transform.position.z);
+	}
+}
